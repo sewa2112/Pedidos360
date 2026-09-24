cc233f90deed324c8ec48e717e9c3d6eb54f6211
Sebastian <se.osoriob@duocuc.cl>
2026-09-24 00:23:58 -0300

    correciones principales del docente

--- pedidos360-backend/src/main/java/cl/duoc/pedidos360/config/SecurityConfig.java
+++ pedidos360-backend/src/main/java/cl/duoc/pedidos360/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package cl.duoc.pedidos360.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -16,10 +18,9 @@ public class SecurityConfig {
             .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/pedidos/**").permitAll()
-                .anyRequest().permitAll()
-            );
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
-
